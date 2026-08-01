0ddf833268aee07d29925987dce49b6af15a4c7c
nurulfa381-ui <nurulfa381@gmail.com>
2026-08-01 08:15:07 +0800

    Add C01_MISSIONS array with computer topics

--- missions.js
+++ missions.js
@@ -1,1 +1,136 @@
+const C01_MISSIONS = [
+  {
+    id: 1,
+    title: "Pengenalan Kepada Komputer",
+    bm: "Pengenalan Kepada Komputer",
+    en: "Introduction to Computers",
+    kp: "kp/kp01/index.html",
+    kt: "kt/kt01/index.html"
+  },
 
+  {
+    id: 2,
+    title: "Fungsi Kerja Sistem Komputer",
+    bm: "Fungsi Kerja Sistem Komputer",
+    en: "Computer System Functions",
+    kp: "kp/kp02/index.html",
+    kt: "kt/kt02/index.html"
+  },
+
+  {
+    id: 3,
+    title: "Perkakasan dan Peralatan Komputer",
+    bm: "Perkakasan dan Peralatan Komputer",
+    en: "Computer Hardware and Equipment",
+    kp: "kp/kp03/index.html",
+    kt: "kt/kt03/index.html"
+  },
+
+  {
+    id: 4,
+    title: "Peralatan Pemasangan Perkakasan Komputer",
+    bm: "Peralatan Pemasangan Perkakasan Komputer",
+    en: "Computer Hardware Installation Tools",
+    kp: "kp/kp04/index.html",
+    kt: "kt/kt04/index.html"
+  },
+
+  {
+    id: 5,
+    title: "Fungsi Komponen Perkakasan Komputer",
+    bm: "Fungsi Komponen Perkakasan Komputer",
+    en: "Functions of Computer Hardware Components",
+    kp: "kp/kp05/index.html",
+    kt: "kt/kt05/index.html"
+  },
+
+  {
+    id: 6,
+    title: "Jenis-Jenis Casing Komputer",
+    bm: "Jenis-Jenis Casing Komputer",
+    en: "Types of Computer Cases",
+    kp: "kp/kp06/index.html",
+    kt: "kt/kt06/index.html"
+  },
+
+  {
+    id: 7,
+    title: "Jenis-Jenis Penggera Amaran Komputer",
+    bm: "Jenis-Jenis Penggera Amaran Komputer",
+    en: "Types of Computer Warning Beeps",
+    kp: "kp/kp07/index.html",
+    kt: "kt/kt07/index.html"
+  },
+
+  {
+    id: 8,
+    title: "Pengenalan Mesej POST",
+    bm: "Pengenalan Mesej Power-On Self Test (POST)",
+    en: "Introduction to Power-On Self-Test Messages",
+    kp: "kp/kp08/index.html",
+    kt: "kt/kt08/index.html"
+  },
+
+  {
+    id: 9,
+    title: "Sistem Pengoperasian dan Pemacu Peranti",
+    bm: "Sistem Pengoperasian dan Pemacu Peranti",
+    en: "Operating Systems and Device Drivers",
+    kp: "kp/kp09/index.html",
+    kt: "kt/kt09/index.html"
+  },
+
+  {
+    id: 10,
+    title: "Pemasangan Aplikasi dan Kemas Kini",
+    bm: "Pemasangan Aplikasi Perisian serta Kemas Kini",
+    en: "Application Installation and Software Updates",
+    kp: "kp/kp10/index.html",
+    kt: "kt/kt10/index.html"
+  },
+
+  {
+    id: 11,
+    title: "Backup dan Verifikasi Keselamatan",
+    bm: "Backup dan Verifikasi Keselamatan",
+    en: "Backup and Security Verification",
+    kp: "kp/kp11/index.html",
+    kt: "kt/kt11/index.html"
+  },
+
+  {
+    id: 12,
+    title: "Pemasangan Peralatan Tambahan Komputer",
+    bm: "Pemasangan Peralatan Tambahan Komputer",
+    en: "Computer Peripheral Installation",
+    kp: "kp/kp12/index.html",
+    kt: "kt/kt12/index.html"
+  },
+
+  {
+    id: 13,
+    title: "Pengujian Sistem Komputer",
+    bm: "Pengujian Sistem Komputer",
+    en: "Computer System Testing",
+    kp: "kp/kp13/index.html",
+    kt: "kt/kt13/index.html"
+  },
+
+  {
+    id: 14,
+    title: "Pengendalian Perkakasan Tambahan",
+    bm: "Pengendalian Perkakasan Tambahan Komputer",
+    en: "Handling Additional Computer Hardware",
+    kp: "kp/kp14/index.html",
+    kt: "kt/kt14/index.html"
+  },
+
+  {
+    id: 15,
+    title: "Penyediaan Laporan Sistem Komputer",
+    bm: "Penyediaan Laporan Sistem Komputer",
+    en: "Preparing Computer System Reports",
+    kp: "kp/kp15/index.html",
+    kt: "kt/kt15/index.html"
+  }
+];
